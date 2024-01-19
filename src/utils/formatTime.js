016bb66ce4f9012634d90308f0875567dd2e8452
Anil Shrestha <anilandshrestha1@gmail.com>
2024-01-19 21:01:41 +0545

    frontend update

--- src/utils/formatTime.js
+++ src/utils/formatTime.js
@@ -1,8 +1,6 @@
 export const formatTime = (time) => {
-  const totalMinutes = 124;
-
-  const hours = Math.floor(totalMinutes / 60);
-  const minutes = totalMinutes % 60;
+  const hours = Math.floor(time / 60);
+  const minutes = time % 60;
   const formatedTime = `${hours}hr ${minutes}min`;
   return formatedTime;
 };
